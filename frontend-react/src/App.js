08a2aaf5d39d70ceb1da0c98a2e7d5e4c81d3abd
P M <pvmannion@gmail.com>
2026-05-26 12:43:49 -0700

    fix: remove basename from HashRouter

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <AuthProvider>
       {/* React Router handles all page navigation */}
-      <HashRouter basename="/the-literary-lounge">
+      <HashRouter>
         <Routes>
           {/* Redirect root to login */}
           <Route path="/" element={<Navigate to="/login" replace />} />
